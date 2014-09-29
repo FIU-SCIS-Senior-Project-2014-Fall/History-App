052c0346a3144db420b78f23e7aae22a719c2847
yyser001 <yyser001>
2014-09-28 21:29:30 -0400

    starting stages of main screen

--- Code/SourceFiles/Android App/app/src/main/java/edu/fiu/seniorproject/historyapp/MainScreenClass.java
+++ Code/SourceFiles/Android App/app/src/main/java/edu/fiu/seniorproject/historyapp/MainScreenClass.java
@@ -3,10 +3,19 @@ package edu.fiu.seniorproject.historyapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainScreenClass extends Activity{
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainScreenClass extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.mainscreenlayout);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.mainscreenlayout);
+
     }
+    //button code information
+    //http://developer.android.com/guide/topics/ui/controls/button.html
 }
