0e04842d0b510f39b587dd7f7dc3f9e04400de0b
yyser001 <yyser001>
2014-10-06 00:12:49 -0400

    Google Map API + Real Devices issues
    Issues getting a real device to run the app; Google Services not running
    on Emulator

--- Code/SourceFiles/Android App/app/src/main/java/edu/fiu/seniorproject/historyapp/SplashScreen.java
+++ Code/SourceFiles/Android App/app/src/main/java/edu/fiu/seniorproject/historyapp/SplashScreen.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
 
 public class SplashScreen extends Activity {
 
